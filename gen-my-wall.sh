88e80a81081a002b46f1a033982e058e90539dda
Naheel-Azawy <naheelazawy@gmail.com>
2018-02-17 21:29:48 +0300

    the thing

--- gen-my-wall.sh
+++ gen-my-wall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./mandelbrot.c wall.ppm 5760 3240 1200 10.3 0.270925 0.004747
+gcc -lm mandelbrot.c -o mandelbrot
+./mandelbrot wall.ppm 5760 3240 1200 10.3 0.270925 0.004747
 convert -resize 1920x1080 wall.ppm wall.png
 rm wall.ppm
